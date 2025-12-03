ad43171257d96dd0b0bd22c3716ba1a91248cf28
krishna.soni@yash.com <krishna.soni@yash.com>
2025-12-03 11:04:31 +0530

    Pivot added for Ll Bp Rc

--- src/webparts/rootCauseAnalysis/components/LL, BP & RC/LlBpRc.tsx
+++ src/webparts/rootCauseAnalysis/components/LL, BP & RC/LlBpRc.tsx
@@ -1,9 +1,23 @@
 import * as React from "react";
+import { Pivot, PivotItem } from "@fluentui/react";
+import LessonsLearnt from "./Lessons Learnt/LessonsLearnt";
+import BestPractices from "./Best Practices/BestPractices";
+import ReusableComponents from "./Reusable Components/ReusableComponents";
 
 const LlBpRc: React.FC = () => {
   return (
     <div>
-        Lessons Learnt, Best Practices & Reusable Components Content
+      <Pivot aria-label="Lessons learnt tabs" defaultSelectedKey="lessonsLearnt">
+        <PivotItem headerText="Lessons Learnt" itemKey="lessonsLearnt">
+          <LessonsLearnt />
+        </PivotItem>
+        <PivotItem headerText="Best Practices" itemKey="bestPractices">
+          <BestPractices />
+        </PivotItem>
+        <PivotItem headerText="Reusable Components" itemKey="reusableComponents">
+          <ReusableComponents />
+        </PivotItem>
+      </Pivot>
     </div>
   );
 };
